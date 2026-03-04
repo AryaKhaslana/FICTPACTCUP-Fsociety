7e46c67ccd8154e70988d9fd5eb1910142708696
Braja Khaslana <aryachakabrajawisesa@gmail.com>
2026-03-04 13:27:21 +0700

    Last hari ini

--- src/app/components/Navbar/index.jsx
+++ src/app/components/Navbar/index.jsx
@@ -4,8 +4,6 @@ import AuthNav from './AuthNav';
 export default function Navbar({ isAuthenticated }) {
   return (
     <nav className="w-full bg-[#1E1E1E] px-6 py-4 flex justify-between items-center">
-      <div className="font-bold text-white text-xl">XPact</div>
-
       <div>
         {isAuthenticated ? <AuthNav /> : <GuestNav />}
       </div>
