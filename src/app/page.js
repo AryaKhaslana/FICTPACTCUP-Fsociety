dd79217e5b20b7069a2b4ea7b416bb1b194ab13b
Braja Khaslana <aryachakabrajawisesa@gmail.com>
2026-03-04 13:09:17 +0700

    fix page

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,9 @@
-import HeroSection from '@/components/LandingPage/HeroSection';
-import FeatureSection from '@/components/LandingPage/FeatureSection';
-import SuccessStory from '@/components/LandingPage/SuccessStory';
-import HowItWorks from '@/components/LandingPage/HowItWorks';
-import CtaSection from '@/components/LandingPage/CtaSection';
 import Navbar from './components/Navbar';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#1E1E1E]">
       <Navbar />
-      <HeroSection />
-      <FeatureSection />
-      <SuccessStory />
-      <HowItWorks />
-      <CtaSection />
     </main>
   );
 }
